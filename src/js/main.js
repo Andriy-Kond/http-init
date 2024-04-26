bd1dfa152aef759d01a546203ba306f8b7b8f701
Andrii <andrii.kondratovich@gmail.com>
2024-04-26 22:22:33 +0300

    add: pagination

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // import usersTpl from "../templates/users.hbs"; // ! не працює
 
-const fetchUsersBtn = document.querySelector(".btn");
+const fetchUsersBtn = document.querySelector(".btnUsers");
 const userList = document.querySelector(".user-list");
 
 const url = "https://jsonplaceholder.typicode.com/users";
@@ -78,3 +78,54 @@ headers.has("Content-Type"); // true
 headers.get("Content-Type"); // "text/bash"
 headers.set("Content-Type", "application/json");
 headers.delete("X-Custom-Header");
+
+// * Пагінація
+const fetchPostsBtn = document.querySelector(".btnPosts");
+const postList = document.querySelector(".posts");
+
+// Controls the group number
+let page = 1;
+// Controls the number of items in the group
+let perPage = 50;
+
+fetchPostsBtn.addEventListener("click", async () => {
+  try {
+    const posts = await fetchPosts();
+    renderPosts(posts);
+    // Increase the group number
+    page += 1;
+
+    // Replace button text after first request
+    if (page > 1) {
+      fetchPostsBtn.textContent = "Fetch more posts";
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+async function fetchPosts() {
+  const params = new URLSearchParams({
+    _limit: perPage,
+    _page: page,
+  });
+
+  const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?${params}`);
+
+  console.log("fetchPosts >> response:::", response);
+  return response.data;
+}
+
+function renderPosts(posts) {
+  const markup = posts
+    .map(({ id, title, body, userId }) => {
+      return `<li>
+          <h2 class="post-title">${title.slice(0, 30)}</h2>
+          <p><b>Post id</b>: ${id}</p>
+          <p><b>Author id</b>: ${userId}</p>
+          <p class="post-body">${body}</p>
+        </li>`;
+    })
+    .join("");
+  postList.insertAdjacentHTML("beforeend", markup);
+}
